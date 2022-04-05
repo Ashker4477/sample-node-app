797cc918dc57bee973fd4860f7eee7938a3a4d63
muhammed Ashker <muhammedashker3@gmail.com>
2022-04-05 19:37:45 +0530

    sample server is ready for host on server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const app = express();
+const Port = process.env.Port || 8080;
+
+app.get('/', (req, res) => {
+    res.send('helloo server is running at', Port);
+});
+
+app.listen(Port, () => console.log('Server is running seccussfully'));
